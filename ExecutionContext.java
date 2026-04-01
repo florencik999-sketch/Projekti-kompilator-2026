850b2a5c53f6088d51eff79b0cd9838c40c508f5
Eris Hasanpapaj <risihasanpapaj@gmail.com>
2026-04-01 19:13:10 +0200

    Execution Content -Erisi

---
+++ ExecutionContext.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExecutionContext {
+    private Map<String, Double> variables = new HashMap<>();
+
+    public void set(String name, double value) {
+        variables.put(name, value);
+    }
+
+    public double get(String name) {
+        if (!variables.containsKey(name)) {
+            throw new RuntimeException("Variabli nuk u gjend" + name);
+        }
+        return variables.get(name);
+    }
+}
